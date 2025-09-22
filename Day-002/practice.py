845cd69f29bb0835601cfcd5c0589401ec1f67e8
sandeep-rankawat <srankawat189@gmail.com>
2025-09-22 18:12:24 +0530

    Pracitce Problem Solved

---
+++ Day-002/practice.py
@@ -0,0 +1,15 @@
+# Practice Question
+# Problem 1: Find Remainder of the number divisible by divisor
+num = int(input("Enter the number: "))
+divisor = int(input("Enter the divisor: "))
+print("The Remainder is ", num%divisor)
+
+# Problem 2: Tell wehther the num is bigger or not
+istrue = num>divisor
+print("The given number is bigger than divisor(True/False): ", istrue)
+
+# Problem 3: Find average of two numbers
+print("The average of two number is ", (num+divisor)/2)
+
+# Problem 4: Calculate the Square of number
+print("The square of the number is ", num*num)

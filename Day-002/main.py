022c3e434e4c82a045e78838b5d327fe18bee5bd
sandeep-rankawat <srankawat189@gmail.com>
2025-09-22 17:51:19 +0530

    Day 2 main.py is added

---
+++ Day-002/main.py
@@ -0,0 +1,16 @@
+# Chapter-4 :
+# Section A:  Type() and Type Conversion
+a = "31.5"
+b = float(a) # converting string to float
+print(type(a))
+print(type(b))
+
+# Section B: Input from user
+name = input("Enter your name: ")
+print(name)
+
+a  = int(input("Enter a number1 "))
+b  = int(input("Enter a number2 "))
+print(a+b)
+
+

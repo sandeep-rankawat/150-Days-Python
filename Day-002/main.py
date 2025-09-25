8e2d1e8803e4d1ceafc97bf9ee600a35c8c3991a
sandeep-rankawat <srankawat189@gmail.com>
2025-09-25 20:35:51 +0530

    Day 2 Completed

--- Day-002/main.py
+++ Day-002/main.py
@@ -13,4 +13,37 @@ a  = int(input("Enter a number1 "))
 b  = int(input("Enter a number2 "))
 print(a+b)
 
+# Chapter-5: String and its method
+a = 'Hello World'
+b = "Good Morning ! Sir,"
+c = '''How Can I help You, Sir?'''
+
+# Section A- String Slicing
+myName = "Sandeep_Rankawat"
+myNameShort = myName[0:7]
+myNameShort = myName[-7:-1]
+myNameShort = myName[:9]
+myNameShort = myName[1:]
+myNameShort = myName[::5]
+print(myNameShort)
+
+# Section B- Funtions and methods
+myName = "sandeep_Rankawat"
+print(len(myName))
+print(myName.endswith("at"))
+print(myName.endswith("ar"))
+print(myName.startswith("san"))
+print(myName.startswith("San"))
+print(myName.capitalize())
+print(myName.upper())
+print(myName.lower())
+print(myName.find("a"))
+print(myName.replace("a", "e"))
+
+# Section C- Escape Sequence Character
+#   \n, \t, \;, \\ etc 
+
+string = "This is first line string. \nThis is second line  string"
+string = "This is first line string. \n\tThis is \"second\" line  string"
+print(string)
 
